990940d81d150a70171aaee1f2a5680b40b12278
Neil Gokul Bhutada <63654394+neilb3@users.noreply.github.com>
2026-06-18 23:37:56 -0500

    add sentiment/lexicon.py

---
+++ sentiment/lexicon.py
@@ -0,0 +1,11 @@
+POSITIVE_WORDS = {
+    "good", "great", "excellent", "amazing", "wonderful", "fantastic",
+    "love", "happy", "best", "awesome", "brilliant", "perfect",
+    "delightful", "pleased", "satisfied", "outstanding", "superb",
+}
+
+NEGATIVE_WORDS = {
+    "bad", "terrible", "awful", "horrible", "worst", "hate",
+    "poor", "disappointing", "sad", "angry", "frustrated", "broken",
+    "useless", "annoying", "fail", "failure", "wrong",
+}
